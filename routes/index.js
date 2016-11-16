9d4d0cee7bf1f15bb731de932b1189483f9f535d
DESKTOP-JH6TVNS\Mikkel <mikkelkaasandersen@gmail.com>
2016-11-16 10:04:31 +0100

    v.0.2.2 Add routes for '/' in app.js Create IndexController.js Create indexRouter

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var indexController = require('../controller/IndexController');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', indexController.index);
 
 module.exports = router;
