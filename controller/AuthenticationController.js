9f1fdf71b8ba89e071071a4e7852f5a550dcbba0
Kasper Markussen <klm@funding-europe.dk>
2016-11-22 18:39:33 +0100

    v0.3.1 add authentication cookies

--- controller/AuthenticationController.js
+++ controller/AuthenticationController.js
@@ -13,17 +13,39 @@ module.exports.authenticateLogin = function (req, res) {
         name: req.body.name
     }, function (err, user){
 
-        // if(err) throw err;
+        if(err) throw err;
         console.log(user);
         if(user && user.password == sha512(req.body.password, user.salt).passwordHash) {
-            var token = jwt.sign(user._doc, config.secret, {expiresIn: '24h'});
+            var token = jwt.sign({
+                sub: user._id,
+                centerId: user.centerId,
+                permissions: function() {
+                    var result;
+                    switch(user.role) {
+                        case 1:
+                            result = "read-errors";
+                            break;
+                        case 2:
+                            result = "write-errors";
+                            break;
+                        case 3:
+                            result = "admin";
+                            break;
+                    }
+                    return result;
+                }()
+            }, config.secret, {expiresIn: '24h'});
             console.log(token);
             // return the information including token as JSON
-            res.json({
+            res.cookie("bowlnfunErrorApp", token, {
+                expires: new Date(Date.now() + (60 * 60 * 24 * 30))
+            })
+                .json({
                 success: true,
                 message: 'Enjoy your token!',
                 token: token
             });
+            console.log(res.cookie);
         } else res.status(401.1).json({
             success: false,
             message: 'Authentication failed. Wrong username or password.'
@@ -33,7 +55,7 @@ module.exports.authenticateLogin = function (req, res) {
 
 module.exports.authenticateToken = function (req, res, next) {
     // check header or url parameters or post parameters for token
-    var token = req.body.token || req.query.token || req.headers['x-access-token'];
+    var token = req.body.token || req.query.token || req.headers['x-access-token'] || req.cookies.bowlnfunErrorApp;
 
     // decode token
     if (token) {
