d1e1c28f63cabdd92f9e0026328b52aa1fd83779
eaamikkelmedetk <eaamiand32@students.eaaa.dk>
2017-09-13 19:16:34 +0200

    Update config.js

--- config.js
+++ config.js
@@ -4,8 +4,8 @@
 
 module.exports = {
 
-    'secret': 'gummiand',
-    'database': 'mongodb://root:gummiand@ds011893.mlab.com:11893/bowlnfun',
+    'secret': 'insertsecrethere',
+    'database': 'databaseconnection',
     'testToken': "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI1ODQwMjc4MTUxZWNhODI1YWFhOTAyZTYiLCJjZW50ZXJJZCI6IjU4NDAyNzJiYmQxOWY4MjFjNGEwZDRiYSIsInBlcm1pc3Npb25zIjoiYWRtaW4scmVhZC1hY2Nlc3Msd3JpdGUtYWNjZXNzIiwiaWF0IjoxNDgxMDgyMzU4LCJleHAiOjE0ODE2ODcxNTh9.x4ZwBPsqxEGN8a39Sv9kfbjBq1eMYBCSdg8mtYW5vxM"
 
 }
